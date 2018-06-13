1536a98652c1ed7a211a501cedc7626151d1b6ae
Ninad Prabhukhanolkar <ninadchess96@gmail.com>
2018-06-13 20:23:33 +0530

    Create Java_Primitives_Ex.java

---
+++ Datatypes/Java_Primitives_Ex.java
@@ -0,0 +1,45 @@
+package com;
+
+public class Java_Primitives_Ex {
+
+	public static void main(String[] args) {
+
+		// ---------------------------------------------
+		byte byteVar = 127;
+		short shortVar = 1212;
+		int intVar = 121212;
+		long longVar = 12121212121212L;
+
+		float floatVar = 12.12F;
+		double doubleVar = 12.12;
+
+		char charVar1 = 'A';
+		char charVar2 = 65;
+		char charVar3 = '\u0041';
+		// System.out.println(charVar3);
+
+		char inrCurrencySymbol = '\u8377';
+		// System.out.println(inrCurrencySymbol);
+
+		boolean b = true;
+
+		// ---------------------------------------------
+
+		// Quiz
+
+		// int v = 10; // decimal
+		// int v = 010; // octal
+		// int v = 0x10; // hexa
+		// int v = 0b1100; // binary
+		// System.out.println(v);
+
+		// ----------------------------------------------
+
+		// JDK 1.7
+
+		int bigValue = 1_00_00_000;
+		
+		//-----------------------------------------------
+	}
+
+}
